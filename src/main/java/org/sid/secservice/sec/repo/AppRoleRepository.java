37f00e03940bdd87cc124cdb20e9e8992cf1b349
youssef <you13ssef@gmail.com>
2020-12-29 11:29:32 +0100

    first commit

---
+++ src/main/java/org/sid/secservice/sec/repo/AppRoleRepository.java
@@ -0,0 +1,8 @@
+package org.sid.secservice.sec.repo;
+
+import org.sid.secservice.sec.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
+    AppRole findByRolename(String rolename);
+}
